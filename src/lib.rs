178c758c7496fb4f4d19b50d615b2165f2254019
Matias Juntunen <matias.juntunen@gmail.com>
2018-03-29 17:57:12 +0300

    Add basic text printing support

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
 #![feature(lang_items)]
+#![feature(unique)]
+#![feature(const_fn)]
+#![feature(ptr_internals)]
 #![no_std]
 
 extern crate rlibc;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+mod vga;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    unsafe {
-        let vga_base_addr = 0xb_8000usize;
-        let vga: &mut [u8; 80*25*2] = &mut *(vga_base_addr as *mut [u8; 80*25*2]);
-        let text = "heh ebin";
-        let mut idx = 160usize;
-        for &c in text.as_bytes() {
-            vga[idx] = c;
-            idx += 2;
-        }
-    }
+    vga::clear();
+    println!("heh");
+    println!("ebin");
 }
 
 #[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
