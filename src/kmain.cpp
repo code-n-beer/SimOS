632f01f07699193031d360c28a4ff4e799a81670
Teemu Sarapisto <tsarpf@gmail.com>
2018-05-10 17:45:12 +0300

    Add line scroll, convert tabs to spaces

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -14,40 +14,40 @@
  
 /* Hardware text mode color constants. */
 enum vga_color {
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-	VGA_COLOR_RED = 4,
-	VGA_COLOR_MAGENTA = 5,
-	VGA_COLOR_BROWN = 6,
-	VGA_COLOR_LIGHT_GREY = 7,
-	VGA_COLOR_DARK_GREY = 8,
-	VGA_COLOR_LIGHT_BLUE = 9,
-	VGA_COLOR_LIGHT_GREEN = 10,
-	VGA_COLOR_LIGHT_CYAN = 11,
-	VGA_COLOR_LIGHT_RED = 12,
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-	VGA_COLOR_LIGHT_BROWN = 14,
-	VGA_COLOR_WHITE = 15,
+    VGA_COLOR_BLACK = 0,
+    VGA_COLOR_BLUE = 1,
+    VGA_COLOR_GREEN = 2,
+    VGA_COLOR_CYAN = 3,
+    VGA_COLOR_RED = 4,
+    VGA_COLOR_MAGENTA = 5,
+    VGA_COLOR_BROWN = 6,
+    VGA_COLOR_LIGHT_GREY = 7,
+    VGA_COLOR_DARK_GREY = 8,
+    VGA_COLOR_LIGHT_BLUE = 9,
+    VGA_COLOR_LIGHT_GREEN = 10,
+    VGA_COLOR_LIGHT_CYAN = 11,
+    VGA_COLOR_LIGHT_RED = 12,
+    VGA_COLOR_LIGHT_MAGENTA = 13,
+    VGA_COLOR_LIGHT_BROWN = 14,
+    VGA_COLOR_WHITE = 15,
 };
  
 static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) 
 {
-	return fg | bg << 4;
+    return fg | bg << 4;
 }
  
 static inline uint16_t vga_entry(unsigned char uc, uint8_t color) 
 {
-	return (uint16_t) uc | (uint16_t) color << 8;
+    return (uint16_t) uc | (uint16_t) color << 8;
 }
  
 size_t strlen(const char* str) 
 {
-	size_t len = 0;
-	while (str[len])
-		len++;
-	return len;
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
 }
  
 static const size_t VGA_WIDTH = 80;
@@ -62,27 +62,41 @@ volatile uint16_t* terminal_buffer;
  
 void terminal_initialize(void) 
 {
-	terminal_row = 0;
-	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
+    terminal_row = 0;
+    terminal_column = 0;
+    terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
+    terminal_buffer = (uint16_t*) 0xB8000;
+    for (size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+    }
 }
  
 void terminal_setcolor(uint8_t color) 
 {
-	terminal_color = color;
+    terminal_color = color;
 }
  
 void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) 
 {
-	const size_t index = y * VGA_WIDTH + x;
-	terminal_buffer[index] = vga_entry(c, color);
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
+}
+ 
+void terminal_scroll_line() {
+    for(size_t row = 1; row < VGA_HEIGHT; row++) {
+        for(size_t col = 0; col < VGA_WIDTH; col++) {
+            const size_t index = row * VGA_WIDTH + col;
+            terminal_putentryat(terminal_buffer[index], terminal_color, col, row - 1);
+
+            if(row == VGA_HEIGHT - 1)
+            {
+                terminal_putentryat(' ', terminal_color, col, row);
+            }
+        }
+    }
 }
  
 void terminal_putchar(char c) 
@@ -90,34 +104,78 @@ void terminal_putchar(char c)
     if(c == '\n') {
         terminal_row++;
         terminal_column = 0;
+        if (terminal_row == VGA_HEIGHT)
+        {
+            terminal_row = VGA_HEIGHT - 1;
+            terminal_scroll_line();
+        }
         return;
     }
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+        terminal_column = 0;
+        if (++terminal_row == VGA_HEIGHT)
+        {
+            terminal_row = VGA_HEIGHT - 1;
+            terminal_scroll_line();
+        }
+    }
 }
- 
+
 void terminal_write(const char* data, size_t size) 
 {
-	for (size_t i = 0; i < size; i++)
-		terminal_putchar(data[i]);
+    for (size_t i = 0; i < size; i++)
+        terminal_putchar(data[i]);
 }
  
 void terminal_writestring(const char* data) 
 {
-	terminal_write(data, strlen(data));
+    terminal_write(data, strlen(data));
+}
+
+char nthdigit(int x, int n)
+{
+    while (n--) {
+        x /= 10;
+    }
+    return (x % 10) + '0';
 }
  
+void print_num(int num)
+{
+    if(num < 10) {
+        char* n = "1\n";
+        n[0] = num + '0';
+        return terminal_writestring(n);
+    }
+
+    int benis = num;
+    unsigned int numDigits = 0;
+    do {
+        ++numDigits;
+        benis /= 10;
+    } while(benis);
+    if(numDigits > 2) 
+    {
+        return terminal_writestring("over 2 digit numbers not supported :D kthx bye\n");
+    }
+
+    char* n = "00\n";
+    n[0] = num / 10 + '0';
+    n[1] = num % 10 + '0';
+    terminal_writestring(n);
+}
 extern "C" void kmain(void) 
 {
-	/* Initialize terminal interface */
-	terminal_initialize();
+    /* Initialize terminal interface */
+    terminal_initialize();
  
-	/* Newline support is left as an exercise. */
-	terminal_writestring("Hello, kernel World!\n");
+    for(int i = 0; i < 30; i++)
+    {
+        terminal_writestring("Hello, kernel World!\n");
+        print_num(i);
+    }
 }
 
 //extern "C" void kmain(const void* multibootHeader)
