ee9beb58cbb512d8482069717bf7774fbd3bbb0b
Matias Juntunen <matias.juntunen@gmail.com>
2018-11-18 17:05:11 +0200

    everything is broken

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -34,19 +34,19 @@ PhysicalAddress virtualToPhysical(void* ptr)
     auto pdidx = PD_IDX_FROM_ADDR(addr);
 
     auto pml4 = reinterpret_cast<const uintptr_t*>(0xffff'ffff'ffff'f000ull + 8 * pml4idx);
-    printf("pml4idx: %lld (%llx)\n", pml4idx, *pml4);
+    //printf("pml4idx: %lld (%llx)\n", pml4idx, *pml4);
     if (!(*pml4 & MMU_FLAG_PRESENT)) {
         return 0;
     }
 
     auto pdpt = reinterpret_cast<const uintptr_t*>(0xffff'ffff'ffe0'0000ull + 0x1000ull * pml4idx);
-    printf("pdptidx: %lld (%llx)\n", pdptidx, *pdpt);
+    //printf("pdptidx: %lld (%llx)\n", pdptidx, *pdpt);
     if (!(*pdpt & MMU_FLAG_PRESENT)) {
         return 0;
     }
 
     auto pd = reinterpret_cast<const uintptr_t*>(0xffff'ffff'c000'0000ull + 0x20'0000ull * pml4idx + 0x1000ull * pdptidx);
-    printf("pdidx: %lld (%llx)\n", pdidx, *pd);
+    //printf("pdidx: %lld (%llx)\n", pdidx, *pd);
     if (!(*pd & MMU_FLAG_PRESENT)) {
         return 0;
     }
@@ -110,7 +110,7 @@ void dumpMultibootInfo(const MultibootBasicInfo* info)
             dumpTag(static_cast<const MmapTag&>(tag));
             break;
         case TagType::ElfSections:
-            dumpTag(static_cast<const ElfSectionsTag&>(tag));
+            //dumpTag(static_cast<const ElfSectionsTag&>(tag));
             break;
         default:
             break;
@@ -118,12 +118,62 @@ void dumpMultibootInfo(const MultibootBasicInfo* info)
     }
 }
 
+static const size_t PAGE_SIZE = 0x20'0000;
+
+enum MmuFlags : uint64_t
+{
+    Mmu_Present = (1 << 0),
+    Mmu_ReadWrite = (1 << 1),
+    Mmu_PageSize = (1 << 7)
+};
+
+void mapPhysical(PhysicalAddress physAddr, void* virtAddr, size_t size, uint64_t flags = 0)
+{
+    // Align to page size
+    size = ((size - 1) & ~(PAGE_SIZE - 1)) + PAGE_SIZE;
+
+    auto addr = reinterpret_cast<uintptr_t>(virtAddr);
+
+    while (size) {
+        auto pml4idx = PML4_IDX_FROM_ADDR(addr);
+        auto pdptidx = PDPT_IDX_FROM_ADDR(addr);
+        auto pdidx = PD_IDX_FROM_ADDR(addr);
+
+        auto pml4 = reinterpret_cast<uint64_t*>(0xffff'ffff'ffff'f000ull + 8 * pml4idx);
+        if (!(*pml4 & Mmu_Present)) {
+            printf("pml4 index %lld not present!\n", pml4idx);
+            return;
+        }
+
+        auto pdpt = reinterpret_cast<uint64_t*>(0xffff'ffff'ffe0'0000ull + 0x1000ull * pml4idx);
+        if (!(*pdpt & Mmu_Present)) {
+            printf("pdpt index %lld not present!\n", pdptidx);
+            return;
+        }
+
+        auto pd = reinterpret_cast<uint64_t*>(0xffff'ffff'c000'0000ull + 0x20'0000ull * pml4idx + 0x1000ull * pdptidx);
+
+        auto pde = (physAddr & (0x3fff'ffffull << 21)) | Mmu_Present | Mmu_PageSize | flags;
+        printf("pde: %016llx\n", pde);
+
+        addr += PAGE_SIZE;
+        physAddr += PAGE_SIZE;
+        size -= PAGE_SIZE;
+    }
+}
+
 extern "C" void kmain(const MultibootBasicInfo* basicInfo)
 {
     console::init();
-
+    // 0000'7ffd'8c79'd000
     uint64_t* virtualPML4 = (uint64_t*)(0xFFFF'FFFF'FFFF'F000ULL);
 
+    //void* p = (void*)(0xffff'ffff'0000'0000ull);
+    void* p = (void*)(0xffff'ffff'0000'0000ull);
+    auto x = (uint64_t)p;
+    //mapPhysical(0x20'0000ull, p, 1 * PAGE_SIZE, Mmu_ReadWrite);
+    printf("virt: %016llx, phys: %016llx\n", (uint64_t)p, virtualToPhysical(p));
+    printf("virt: ffffffff8040a000, phys: %016llx\n", virtualToPhysical((void*)0xffff'ffff'8040'a000ull));
     dumpMultibootInfo(basicInfo);
     
     __asm__("hlt");
