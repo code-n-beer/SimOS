0b3f8fb7710cac661f95d5d29dd9e5bc9e254a67
Matias Juntunen <matias.juntunen@gmail.com>
2019-02-05 18:13:58 +0200

    remove lambda stuff from main

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -76,22 +76,5 @@ extern "C" void kmain(const multiboot::Info* info)
     console::init();
     paging::init(info);
 
-    int x = 5;
-
-    auto fn = [&x]() {
-        printf("hello from fn (%d)\n", x);
-    };
-
-    stl::Lambda<void()> lambda([&x]() {
-        printf("hello from lambda (%d)\n", x);
-    });
-
-    stl::Lambda<void()> lambda2(fn);
-
-    lambda();
-    x = 10;
-    lambda();
-    lambda2();
-
     __asm__("hlt");
 }
